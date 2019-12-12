0b5367a726c117f652d5e843c6c470cee6737e6e
ssjsrgx <ssjsrgx@gmail.com>
2019-12-12 17:27:57 +0100

    ey

--- Proyecto_Final/datasetPreparation.py
+++ Proyecto_Final/datasetPreparation.py
@@ -6,34 +6,43 @@ import numpy as np
 def carga_csv(file_name):
     valores = read_csv(file_name, header=None).values
     # suponemos que siempre trabajaremos con float
-    return valores
+    return valores[1:]
 
 
 def main():
-    trainingSet = carga_csv("train.csv")
-    trainingSetLen = len(trainingSet)  # 2001 casos de entrenamiento
+    datos = carga_csv("train.csv")
+    # Eliminamos la primera fila con los atributos del dataset
 
-    # Utilizamos el 30% de los ejemplos de entrenamiento como conjunto de validación
-    n_cv_set = int(trainingSetLen*0.3)
-    n_training_set = int(trainingSetLen*0.7)
+    datosLen = len(datos)  # 2001 casos de entrenamiento
 
-    processedTrainingSet = trainingSet[:n_training_set]
-    processedValidationSet = trainingSet[-n_cv_set:]
+    # Cogemos el 60% de los datos set de entrenamiento
+    n_training = int(datosLen*0.6)
 
-    # Tamaño del set de entrenamiento a utilizar: 1400 casos
-    print(len(processedTrainingSet))
-    # Tamaño del set de validación a utilizar: 600 casos
-    print(len(processedValidationSet))
+    training_set = datos[:n_training]
+
+    # Por otro lado el 20% para el set de validacion
+    n_validation = int(datosLen*0.2)
+
+    validation_set = datos[n_training:n_training+n_validation]
+
+    # Por ultimo el 20% restante para el conjunto de prueba
+    n_test = datosLen - n_training - n_validation
+
+    test_set = datos[-n_test:]
 
     with open('ProcessedDataSet/train.csv', mode='w', newline='') as processedTraining:
         processedTrainingWriter = csv.writer(
             processedTraining, delimiter=',')
-        processedTrainingWriter.writerows(processedTrainingSet)
+        processedTrainingWriter.writerows(training_set)
 
     with open('ProcessedDataSet/validation.csv', mode='w', newline='') as processedValidation:
         processedValidationWriter = csv.writer(
             processedValidation, delimiter=',')
-        processedValidationWriter.writerows(processedValidationSet)
+        processedValidationWriter.writerows(validation_set)
+
+    with open('ProcessedDataSet/test.csv', mode='w', newline='') as processedTest:
+        processedTestWriter = csv.writer(processedTest, delimiter=',')
+        processedTestWriter.writerows(test_set)
 
 
 main()
